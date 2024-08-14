a33ef05c65074e4ae0f57c0714352836f31d4c88
SAJAL RASTOGI <sajalrastogi795@gmail.com>
2024-08-14 12:03:25 +0530

    updated cors

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://farmen.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 // Serving static files
 app.use("/uploads", express.static(join(__dirname, "/uploads")));
